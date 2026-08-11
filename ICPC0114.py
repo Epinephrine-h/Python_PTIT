d9b6573ea3c4dbda3939a7554c7cf9e3bf7b9fe3
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-08-11 23:21:10 +0700

    Add files via upload

---
+++ ICPC0114.py
@@ -0,0 +1,33 @@
+import math
+def isPrime(n):
+    if n <= 1:      return False
+    if n == 2:      return True
+    if n % 2 == 0:      return False
+    for num in range(3, int(math.sqrt(n) + 1), 2):
+        if n % num == 0:    return False
+    return True
+def reverseNum(n):
+    ans = 0
+    while n:
+        ans +=  n % 10
+        n //= 10
+    return ans
+def checkMember(n):
+    while n:
+        x = n % 10
+        if x != 2 and x != 3 and x != 5 and x != 7:    return False
+        n //= 10
+    return True
+testcase = int(input())
+for _ in range(testcase):
+    n = int(input())
+    if not checkMember(n):
+        print("No")
+        continue
+    if not isPrime(n):
+        print("No")
+        continue
+    if not isPrime(reverseNum(n)):
+        print("No")
+        continue
+    print("Yes")
