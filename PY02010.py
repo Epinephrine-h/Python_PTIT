f9ca3f8755418d46cdb1d5a2886f6a6bc9049812
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-08-14 10:42:08 +0700

    Update PY02010.py

--- PY02010.py
+++ PY02010.py
@@ -1,11 +1,6 @@
 while True:
     n = int(input())
     if n == 0: break
-    small, big = float('inf'), float('-inf')
-    for _ in range(n):
-        num = input()
-        i = 0
-        while i < len(num) and num[i] == '0': i += 1
-        if i == len(num):   num = 0
-        small, big = min(small, int(num)), max(big, int(num))
-    print("BANG NHAU" if small == big else f"{small} {big}")
+    arr = [int(input()) for _ in range(n)]
+    arr.sort()
+    print(f"{arr[0]} {arr[-1]}" if arr[0] != arr[-1] else "BANG NHAU")
