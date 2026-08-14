ca39fdaa670e11e50b515e63e1a6cfd053d049ed
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-08-15 00:41:59 +0700

    Update PY01022.py

--- PY01022.py
+++ PY01022.py
@@ -1,7 +1,6 @@
 n = input()
 def cnt(n):
     if len(n) == 1:     return 0
-    total = 0
-    for ch in n:    total += ord(ch) - ord('0')
+    total = sum(ord(ch) - 48 for ch in n)
     return 1 + cnt(str(total))
 print(1 if len(n) == 1 else cnt(n))
