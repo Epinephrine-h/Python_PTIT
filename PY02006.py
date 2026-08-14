fcf1f6c212dff600910d2f21b1d8e2e7642c9735
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-08-14 10:00:57 +0700

    Update PY02006.py

--- PY02006.py
+++ PY02006.py
@@ -1,11 +1,7 @@
-def check(a, b):
-    for i in range(len(a)):
-        if a[i] > b[i]:     return False
-    return True
 testcase = int(input())
 for _ in range(testcase):
     n = int(input())
     a = list(map(int, input().split()))
     b = list(map(int, input().split()))
-    print("YES" if check(sorted(a), sorted(b)) else "NO")
+    print("YES" if all(x <= y for x, y in zip(sorted(a), sorted(b))) else "NO")
 
