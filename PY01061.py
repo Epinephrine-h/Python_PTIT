c3d4216ca0c12db963d8f9f17bf523f395853cee
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-08-15 15:17:02 +0700

    Add files via upload

---
+++ PY01061.py
@@ -0,0 +1,12 @@
+import math
+def isPrime(n):
+    if n <= 1:  return False
+    if n == 2:  return True
+    if n % 2 == 0:  return False
+    for num in range(3, int(math.sqrt(n) + 1), 2):
+        if n % num == 0:    return False
+    return True
+testcase = int(input())
+for _ in range(testcase):
+    s = input()
+    print("YES" if (isPrime(int(s[:3])) and isPrime(int(s[-3:]))) else "NO")
