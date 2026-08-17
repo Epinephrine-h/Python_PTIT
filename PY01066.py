fc8683654ecfeed7f5e7ca5f1b0dcbbbe8a0ed3c
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-08-17 12:41:25 +0700

    Add files via upload

---
+++ PY01066.py
@@ -0,0 +1,9 @@
+def check(s):
+    i = 1
+    while i < len(s):
+        if abs(ord(s[i]) - ord(s[i-1])) != abs(ord(s[-i]) - ord(s[-i-1])):   return False
+        i += 1
+    return True
+testcase = int(input())
+for _ in range(testcase):
+    print("YES" if check(input()) else "NO")
