4c06559067a2f0fd3198d3ba686d4b2a006b9be4
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-08-18 23:19:31 +0700

    Add files via upload

---
+++ PY02040.py
@@ -0,0 +1,11 @@
+n = int(input())
+matrix = [list(map(int,input().split())) for _ in range(n)]
+k = int(input())
+up = down = 0
+for i in range(n):
+    for j in range(n):
+        if i + j < n - 1:   down += matrix[i][j]
+        if i + j > n - 1:   up += matrix[i][j]
+deviation = abs(up - down)
+print("YES" if deviation <= k else "NO")
+print(deviation)
