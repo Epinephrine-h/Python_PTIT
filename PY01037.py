472d40fee135d2f245eeecd79115e3433146592e
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-08-21 00:19:50 +0700

    Add files via upload

--- PY01037.py
+++ PY01037.py
@@ -5,15 +5,17 @@ def main():
         15120, 20160, 25200, 27720, 45360, 50400, 55440, 83160, 110880, 166320, 221760, 277200, 332640, 498960, 
         554400, 665280, 720720, 1081080, 1441440, 2162160, 2882880, 3603600, 4324320, 6486480, 7207200, 8648640, 
         10810800, 14414400]
-    testcase = int(input())
-    t = 0
-    for line in sys.stdin:
+    input_line = sys.stdin.readline
+    first_line = input_line()
+    testcase = int(first_line.strip())
+    for _ in range(testcase):
+        line = input_line()
+        if not line:
+            break
         n = int(line)
         pos = bisect.bisect_left(antiprimes, n)
         print(antiprimes[pos])
-        t+=1
-        if t == testcase:   break
-if __name__ == '__main__':# Add this fucking line unless you wanna enjoy Time Limit Exceeded
+if __name__ == '__main__':
     main()
 """use this to find antiprimes
 candidates = [(1,1)]
