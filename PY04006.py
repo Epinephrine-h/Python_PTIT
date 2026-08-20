0c3c2371500d9b115fefcef6020a49623473e22c
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-08-20 11:13:34 +0700

    Add files via upload

---
+++ PY04006.py
@@ -0,0 +1,33 @@
+import sys
+import math
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def distance(self, other):
+        return math.sqrt((self.x - other.x)**2 + (self.y - other.y)**2)
+class Triangle:
+    def __init__(self, p1, p2, p3):
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+    def get_area(self):
+        a = self.p1.distance(self.p2)
+        b = self.p1.distance(self.p3)
+        c = self.p2.distance(self.p3)
+        if a + b <= c or a + c <= b or b + c <= a:
+            return "INVALID"
+        p = (a + b + c) / 2
+        area_sqr = p * (p - a) * (p - b) * (p - c)
+        area = math.sqrt(area_sqr)
+        
+        return f"{area:.2f}"
+data = sys.stdin.read().split()
+testcase = int(data[0])
+idx = 1
+for _ in range(testcase):
+    coords = [float(x) for x in data[idx:idx+6]]
+    idx+=6
+    p1, p2, p3 = Point(coords[0], coords[1]), Point(coords[2], coords[3]), Point(coords[4], coords[5])
+    x = Triangle(p1,p2,p3)
+    print(x.get_area())
