372dbaeb746081d08d1be8323a6e4d51d98772a2
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-08-22 19:30:35 +0700

    Update PYKT058.py

--- PYKT058.py
+++ PYKT058.py
@@ -1,19 +1,18 @@
 testcase = int(input())
 for _ in range(testcase):
-    n, m , u, v = map(int, input().split())
-    graph = [[] for vertex in range(n + 1)]
-    for i in range(m):
+    n, m, u, v = map(int, input().split())
+    graph = [[] for vertex in range(n+1)]
+    for e in range(m):
         x, y = map(int, input().split())
         graph[x].append(y)
-    visited = [False] * (n + 1)
-    def dfs(u, ban):
-        visited[u] = True
-        for v in graph[u]:
-            if v != ban and not visited[v]:     dfs(v, ban)
+    def dfs(cur_node, banned_node):
+        visited[cur_node] = True
+        for next_node in graph[cur_node]:
+            if next_node != banned_node and not visited[next_node]:     dfs(next_node, banned_node)
     ans = 0
     for i in range(1, n + 1):
         if i == u or i == v:    continue
-        for x in range(n + 1):  visited[x] = False
+        visited = [False]*(n+1)
         dfs(u, i)
-        if not visited[v]:      ans+=1
+        if not visited[v]:  ans+=1
     print(ans)
