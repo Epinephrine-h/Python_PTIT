d392ed7b1da5dd5c7b14c46b192955af33fa3648
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-09-13 23:17:31 +0700

    Add files via upload

---
+++ PYKT084.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+m = {}
+t, cnt = None, 0
+for _ in range(n):
+    s = input().strip()
+    if not t:
+        t = s
+        continue
+    if not s:
+        m[t] = cnt
+        t = None
+        cnt = 0
+    else:
+        cnt+=1
+if t: m[t] = cnt
+for topic, amount in m.items():
+    print(topic,": ", amount, sep = '')
